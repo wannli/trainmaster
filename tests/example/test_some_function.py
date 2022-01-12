43d783bae2336d8ca54048c4629c4bd9057eb5c3
Wannes <57009124+wannli@users.noreply.github.com>
2022-01-11 19:50:43 -0500

    add tests

---
+++ tests/example/test_some_function.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+@pytest.mark.parametrize(('first', 'second', 'expected'), [
+    (1, 2, 3),
+    (2, 4, 6),
+    (-2, -3, -5),
+    (-5, 5, 0),
+])
+def test_some_function(first, second, expected):
+    """Example test with parametrization."""
+    assert first + second == expected
