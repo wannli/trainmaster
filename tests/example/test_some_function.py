f19fe720a804b9ea9cde11d5ea9e2f89cd3b6dd0
Wannes Lint <contact@wanneslint.net>
2022-01-16 11:05:23 -0500

    update test station

--- tests/example/test_some_function.py
+++ tests/example/test_some_function.py
@@ -19,21 +19,31 @@ def test_some_function(first, second, expected):
     assert first + second == expected
 
 
-def test_permit_entry_and_reserve_and_enter():
-    too_small = Station(1, 1, id="Breda")
-    train = Train(x=10, y=5, id="SprinterTest")
-    train2 = Train(x=10, y=5, id="SprinterTest2")
-    assert too_small.permit_entry() == True
-    if too_small.permit_entry():
-        train.reserve(too_small)
+@pytest.mark.parametrize(
+    ("depth", "trains"),
+    [(1, 3), (3, 1), (1, 1), (2, 2), (2, 0), (0, 2), (0, 0)],
+)
+def test_station_permit_reserve_enter(depth, trains):
+    station = Station(1, 1, id="Breda", depth=depth)
+    trains = [Train(x=10, y=5, id=f"Test{_}") for _ in range(trains)]
 
-    assert train in too_small.reserved
-    assert too_small.permit_entry() == False
-    train.enter(too_small)
-    assert train in too_small.hold
-    assert train not in too_small.reserved
-    assert too_small.permit_entry() == False
-    if too_small.permit_entry():
-        train2.reserve(too_small)
-    assert train2 not in too_small.hold
-    assert too_small.permit_entry() == False
+    if depth > 0:
+        assert station.permit_entry() == True
+    else:
+        assert station.permit_entry() == False
+
+    for i, train in enumerate(trains, start=1):
+        if station.permit_entry():
+            assert train not in station.reserved
+            assert train not in station.hold
+            train.reserve(station)
+            assert train in station.reserved
+            train.enter(station)
+            assert train in station.hold
+            assert train not in station.reserved
+
+        if depth > i:
+            assert station.permit_entry() == True
+
+        else:
+            assert station.permit_entry() == False
