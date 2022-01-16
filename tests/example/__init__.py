22b760b0d293bee20ea32b8b5c05baeca2566614
Wannes Lint <contact@wanneslint.net>
2022-01-15 20:54:24 -0500

    add package to sys.path for pytest

---
+++ tests/example/__init__.py
@@ -0,0 +1,4 @@
+import sys
+
+sys.path.insert(0, "/Users/lt/sandbox/trainmaster/")
+from trainmaster.main import *
--- tests/example/test_some_function.py
+++ tests/example/test_some_function.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+
 from trainmaster.main import *
 
 # from trainmaster import Station, Train
